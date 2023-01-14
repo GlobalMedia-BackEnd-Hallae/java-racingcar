adbb41d42b08c2e91808fcf70046854da85ecb30
Sy P <yongsa0221@gmail.com>
2023-01-15 01:29:05 +0900

    feat : userInput 메서드 추가

--- src/main/java/gmbs/view/Display.java
+++ src/main/java/gmbs/view/Display.java
@@ -24,7 +24,11 @@ public class Display {
         System.out.println("우승자는,,,,    " + winnerNames);
     }
 
-    public void showInputDisplay() {
+    public void showStartDisplay() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void showRepeatDisplay() {
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 }
