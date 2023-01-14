f8bcc68a39d14fcd2e0598c425707b599d650f0d
Chan531 <chan657312@gmail.com>
2023-01-14 21:23:12 +0900

    Car 클래스 형식의 리스트 생성 테스트 (오류가 나는데 잘 모르겠음)

---
+++ src/test/java/Model/CreateCarClassListTest.java
@@ -0,0 +1,35 @@
+package Model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CreateCarClassListTest {
+    private CreateCarClassList createCarClassList = new CreateCarClassList();
+
+    @Test
+    @DisplayName("Car 클래스를 원소로 가지는 리스트를 생성할 수 있다.")
+    void canCreateCarClassList() {
+        // when
+        List<String> carList = new ArrayList<>();
+        carList.add("a");
+        carList.add("b");
+        carList.add("c");
+
+        List<Car> carClassList = new ArrayList<>();
+        carClassList.add(new Car("a"));
+        carClassList.add(new Car("b"));
+        carClassList.add(new Car("c"));
+
+        // given
+
+        // then
+        assertThat(createCarClassList.createCarClassList(carList)).isEqualTo(carClassList);
+
+        // 왜 오류가 나는 거지?? ㅠㅠ
+    }
+}
