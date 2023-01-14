1be5af9b1c5149f7494110d25f5a0d29a0fc3dc2
Sy P <yongsa0221@gmail.com>
2023-01-15 01:26:14 +0900

    feat : userInput 클래스 추가

---
+++ src/main/java/gmbs/view/UserInput.java
@@ -0,0 +1,11 @@
+package gmbs.view;
+
+import java.util.Scanner;
+
+public class UserInput {
+    private static final Scanner userInputScanner = new Scanner(System.in);
+
+    public String userInput() {
+        return userInputScanner.nextLine();
+    }
+}
