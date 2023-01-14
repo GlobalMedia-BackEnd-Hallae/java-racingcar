99db01932ce7d116b547547ef5e361043eaba1e5
Sy P <yongsa0221@gmail.com>
2023-01-15 01:23:00 +0900

    feat : inputManager 클래스 추가

---
+++ src/main/java/gmbs/controller/InputManager.java
@@ -0,0 +1,34 @@
+package gmbs.controller;
+
+import gmbs.model.InputValidator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class InputManager {
+    private static final InputValidator validator = new InputValidator();
+
+    private static void nameOverflowException() throws IllegalArgumentException {
+        throw new IllegalArgumentException("[Error] 이름은 1자 이상 5자 이하여야 한다.");
+    }
+
+    private void illegalRepetitionInputException() throws IllegalArgumentException {
+        throw new IllegalArgumentException("[Error] 잘못된 반복횟수");
+    }
+
+    public List<String> carNameInput(String userInput) {
+        List<String> carNames = new ArrayList<>(Arrays.asList(userInput.split(",")));
+        if (!validator.isValidNames(carNames)) {
+            nameOverflowException();
+        }
+        return carNames;
+    }
+
+    public int repetitionInput(String userInput) {
+        if (!validator.isNaturalNumber(userInput)) {
+            illegalRepetitionInputException();
+        }
+        return Integer.parseInt(userInput);
+    }
+}
