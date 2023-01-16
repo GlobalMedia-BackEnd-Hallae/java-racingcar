921c3cfa8450bc3ef94b8d8c29cf943ed00a4705
chanwoo <chocw0402@gmail.com>
2023-01-17 00:16:57 +0900

    Main 클래스

--- src/main/java/gmbs/Main.java
+++ src/main/java/gmbs/Main.java
@@ -1,11 +1,20 @@
 package gmbs;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import gmbs.controller.RacingGameController;
+import gmbs.view.input.InputConsole;
+import gmbs.view.input.InputConsoleImpl;
+import gmbs.view.output.OutputConsole;
+import gmbs.view.output.OutputConsoleImpl;
 
-    public String a() {
-        return "abc";
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        InputConsole inputConsole =  new InputConsoleImpl(scanner);
+        OutputConsole outputConsole = new OutputConsoleImpl();
+        RacingGameController racingGameController = new RacingGameController(inputConsole, outputConsole);
+        racingGameController.run();
     }
 }
