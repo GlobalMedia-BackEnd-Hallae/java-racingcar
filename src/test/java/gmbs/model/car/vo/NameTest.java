976ca750ed9837e193173a1845f5b2fb9facad36
chanwoo <chocw0402@gmail.com>
2023-01-17 00:17:52 +0900

    test : Name VO 클래스 테스트

---
+++ src/test/java/gmbs/model/car/vo/NameTest.java
@@ -0,0 +1,42 @@
+package gmbs.model.car.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NameTest {
+
+    private static final String OVER_THAN_MAX_LENGTH = "SixLen";
+    private static final String BLANK = " ";
+
+    @DisplayName("정상적인 이름이 들어오면 생성에 성공한다")
+    @ParameterizedTest
+    @CsvSource({"벤틀리, 벤틀리", "람보르기니, 람보르기니"})
+    void createName(String carName, String expect) {
+        // when
+        Name name = new Name(carName);
+
+        // then
+        assertThat(name.getValue()).isEqualTo(expect);
+    }
+
+    @DisplayName("5자가 넘는 자동차 이름이 들어오면 예외를 발생시킨다")
+    @Test
+    void throwErrorWhenValueLengthIsLongerThanMaxLength() {
+        assertThatThrownBy(() -> new Name(OVER_THAN_MAX_LENGTH))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름은 5자를 넘길 수 없습니다");
+    }
+
+    @DisplayName("자동차 이름이 공백이면 예외를 발생시킨다")
+    @Test
+    void throwErrorWhenValueIsBlank() {
+        assertThatThrownBy(() -> new Name(BLANK))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름은 공백일 수 없습니다. 다시 입력하세요");
+    }
+}
