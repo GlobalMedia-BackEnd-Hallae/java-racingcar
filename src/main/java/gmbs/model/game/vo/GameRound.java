01a5718fe71f15b459b7adfadfed6964df0c1869
chanwoo <chocw0402@gmail.com>
2023-01-17 00:11:18 +0900

    feat : GameRound VO 클래스

---
+++ src/main/java/gmbs/model/game/vo/GameRound.java
@@ -0,0 +1,37 @@
+package gmbs.model.game.vo;
+
+public class GameRound {
+
+    private static final int DEFAULT_START_ROUND = 1;
+
+    private final int totalRound;
+    private int currentRound;
+
+    public GameRound(final int totalRound) {
+        checkRound(totalRound);
+        this.totalRound = totalRound;
+        this.currentRound = DEFAULT_START_ROUND;
+    }
+
+    private void checkRound(int totalRound) throws IllegalArgumentException {
+        if (totalRound <= DEFAULT_START_ROUND) {
+            throw new IllegalArgumentException("[ERROR] 시도 회수는 1보다 작을 수 없습니다");
+        }
+    }
+
+    public void next() {
+        currentRound++;
+    }
+
+    public boolean isNotOver() {
+        return currentRound <= totalRound;
+    }
+
+    public int getTotalRoundForTest() {
+        return totalRound;
+    }
+
+    public int getCurrentRoundForTest() {
+        return currentRound;
+    }
+}
