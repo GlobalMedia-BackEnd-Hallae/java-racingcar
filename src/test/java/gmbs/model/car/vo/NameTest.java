872bf8ffd81852e4b46a492127b1966012b54ce8
chanwoo <chocw0402@gmail.com>
2023-01-19 17:34:12 +0900

    refactor : when, then 주석 추가 및 테스트명 변경

--- src/test/java/gmbs/model/car/vo/NameTest.java
+++ src/test/java/gmbs/model/car/vo/NameTest.java
@@ -13,7 +13,7 @@ class NameTest {
     private static final String OVER_THAN_MAX_LENGTH = "SixLen";
     private static final String BLANK = " ";
 
-    @DisplayName("정상적인 이름이 들어오면 생성에 성공한다")
+    @DisplayName("이름의 길이가 5자 이하면 그 이름을 갖는 Name 객체가 생성된다")
     @ParameterizedTest
     @CsvSource({"벤틀리, 벤틀리", "람보르기니, 람보르기니"})
     void createName(String carName, String expect) {
@@ -27,6 +27,7 @@ class NameTest {
     @DisplayName("5자가 넘는 자동차 이름이 들어오면 예외를 발생시킨다")
     @Test
     void throwErrorWhenValueLengthIsLongerThanMaxLength() {
+        // when, then
         assertThatThrownBy(() -> new Name(OVER_THAN_MAX_LENGTH))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 자동차 이름은 5자를 넘길 수 없습니다");
@@ -35,6 +36,7 @@ class NameTest {
     @DisplayName("자동차 이름이 공백이면 예외를 발생시킨다")
     @Test
     void throwErrorWhenValueIsBlank() {
+        // when, then
         assertThatThrownBy(() -> new Name(BLANK))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 자동차 이름은 공백일 수 없습니다. 다시 입력하세요");
