0d2e856571b590defa5e824998ad7945d1119313
Chan531 <chan657312@gmail.com>
2023-01-20 18:03:21 +0900

    메서드 순서 수정

--- src/main/java/View/NumberInput.java
+++ src/main/java/View/NumberInput.java
@@ -6,12 +6,12 @@ public class NumberInput {
 
     private Scanner scanner = new Scanner(System.in);
 
-    public void reportError() {
-        System.out.println("[ERROR] 다시 입력해주세요.");
-    }
-
     public int inputNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
         return scanner.nextInt();
+    }
+
+    public void reportError() {
+        System.out.println("[ERROR] 다시 입력해주세요.");
     }
 }
