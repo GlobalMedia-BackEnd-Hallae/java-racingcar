b2bbdea555d8b753b4560556a154f2160fb9eb09
Chan531 <chan657312@gmail.com>
2023-01-20 18:00:07 +0900

    메서드 순서 수정

--- src/main/java/View/CarInput.java
+++ src/main/java/View/CarInput.java
@@ -6,12 +6,12 @@ public class CarInput {
 
     private Scanner scanner = new Scanner(System.in);
 
-    public void reportError() {
-        System.out.println("[ERROR] 다시 입력해주세요.");
-    }
-
     public String inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return scanner.nextLine();
+    }
+
+    public void reportError() {
+        System.out.println("[ERROR] 다시 입력해주세요.");
     }
 }
