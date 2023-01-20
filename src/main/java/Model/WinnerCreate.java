8ed6500b0d0b3b88073c20493b261ddebc4c5cc9
Chan531 <chan657312@gmail.com>
2023-01-20 18:05:34 +0900

    List를 내부에서 선언해서 하려했으나 실패한 우승자 리스트 생성 클래스. 주석에서 막혔으나 너무 시간이 소모되어서 List를 외부에서 선언한 코드로 커밋합니다.

--- src/main/java/Model/WinnerCreate.java
+++ src/main/java/Model/WinnerCreate.java
@@ -7,8 +7,20 @@ public class WinnerCreate {
 
     List<String> winner = new ArrayList<>();
 
+    public List<String> createWinner(List<Car> carStatus) {
+        int maxDistance = maxDistance(carStatus);
+
+        for (Car car : carStatus) {
+            isWinner(maxDistance, car.carPosition(), car.carName());
+        }
+
+        return winner;
+    }
+
     private int maxDistance(List<Car> carStatus) {
-        int maxValue = 0;
+        final int INITIAL_VALUE = 0;
+
+        int maxValue = INITIAL_VALUE;
 
         for (Car car : carStatus) {
             maxValue = Math.max(car.carPosition(), maxValue);
@@ -23,13 +35,34 @@ public class WinnerCreate {
         }
     }
 
-    public List<String> createWinner(List<Car> carStatus) {
+    /*public List<String> createWinner(List<Car> carStatus) {
+        List<String> winner = new ArrayList<>();
         int maxDistance = maxDistance(carStatus);
 
         for (Car car : carStatus) {
-            isWinner(maxDistance, car.carPosition(), car.carName());
+            winner.add(isWinner(maxDistance, car.carPosition(), car.carName()));
         }
 
         return winner;
     }
+
+    private int maxDistance(List<Car> carStatus) {
+        final int INITIAL_VALUE = 0;
+
+        int maxValue = INITIAL_VALUE;
+
+        for (Car car : carStatus) {
+            maxValue = Math.max(car.carPosition(), maxValue);
+        }
+
+        return maxValue;
+    }
+
+    private String isWinner(int maxDistance, int carDistance, String carName) {
+        if (maxDistance == carDistance) {
+            return carName;
+        }
+
+        return null;
+    }*/
 }
