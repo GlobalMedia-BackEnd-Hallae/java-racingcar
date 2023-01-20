8b4e6ca4c9237d2909fa2eb9032cca5b71cb1201
Chan531 <chan657312@gmail.com>
2023-01-20 18:02:51 +0900

    원시값 포장을 하고 있는 carName. carName의 조건에 부합하지 않다면 오류를 냄

--- src/main/java/Model/CarName.java
+++ src/main/java/Model/CarName.java
@@ -5,6 +5,12 @@ public class CarName {
     private final String name;
 
     public CarName(String name) {
+        final int CAR_NAME_LIMIT = 5;
+
+        if (name.length() > CAR_NAME_LIMIT) {
+            throw new IllegalStateException();
+        }
+
         this.name = name;
     }
 
