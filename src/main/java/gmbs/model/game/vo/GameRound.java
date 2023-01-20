1bffe8deacf5b32d2b4fe6beeb6115f800d7eefe
chanwoo <chocw0402@gmail.com>
2023-01-20 14:53:41 +0900

    refactor : equalsAndHashCode 추가

--- src/main/java/gmbs/model/game/vo/GameRound.java
+++ src/main/java/gmbs/model/game/vo/GameRound.java
@@ -1,5 +1,7 @@
 package gmbs.model.game.vo;
 
+import java.util.Objects;
+
 public class GameRound {
 
     private static final int DEFAULT_START_ROUND = 1;
@@ -13,7 +15,7 @@ public class GameRound {
         this.currentRound = DEFAULT_START_ROUND;
     }
 
-    private void checkRound(int totalRound) throws IllegalArgumentException {
+    private void checkRound(int totalRound) {
         if (totalRound <= DEFAULT_START_ROUND) {
             throw new IllegalArgumentException("[ERROR] 시도 회수는 1보다 작을 수 없습니다");
         }
@@ -33,5 +35,18 @@ public class GameRound {
 
     public int getCurrentRoundForTest() {
         return currentRound;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameRound gameRound = (GameRound) o;
+        return totalRound == gameRound.totalRound && currentRound == gameRound.currentRound;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(totalRound, currentRound);
     }
 }
