5b64d11de69c6b1d6e645cce2e7bc0429df7e054
chanwoo <chocw0402@gmail.com>
2023-01-20 14:52:04 +0900

    refactor : equalsAndHashCode 추가

--- src/main/java/gmbs/model/car/vo/Position.java
+++ src/main/java/gmbs/model/car/vo/Position.java
@@ -1,5 +1,7 @@
 package gmbs.model.car.vo;
 
+import java.util.Objects;
+
 public class Position {
 
     private static final int DEFAULT_POSITION = 0;
@@ -28,5 +30,18 @@ public class Position {
 
     public int getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return value == position.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
