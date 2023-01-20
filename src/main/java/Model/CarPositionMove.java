04f1b26a4f2d4480f5883f7f8d8751d5366927b0
Chan531 <chan657312@gmail.com>
2023-01-20 18:07:21 +0900

    메서드 순서 정리 및 코드 간결화

--- src/main/java/Model/CarPositionMove.java
+++ src/main/java/Model/CarPositionMove.java
@@ -5,18 +5,18 @@ import java.util.Random;
 
 public class CarPositionMove {
 
-    private static final int NUMBER_RANGE = 10;
-    private static final int ADVANCE_LIMIT = 4;
-
     private Random random = new Random();
 
-    private boolean ableToAdvance() {
-        return random.nextInt(NUMBER_RANGE) >= ADVANCE_LIMIT;
+    public void move(List<Car> carStatus) {
+        for (Car car : carStatus) {
+            car.move(ableToAdvance());
+        }
     }
 
-    public void move(List<Car> carClassList) {
-        for (int index = 0; index < carClassList.size(); index++) {
-            carClassList.get(index).move(ableToAdvance());
-        }
+    private boolean ableToAdvance() {
+        final int NUMBER_RANGE = 10;
+        final int ADVANCE_LIMIT = 4;
+
+        return random.nextInt(NUMBER_RANGE) >= ADVANCE_LIMIT;
     }
 }
