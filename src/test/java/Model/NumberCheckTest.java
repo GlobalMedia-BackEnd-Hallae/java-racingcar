b0aae08cd46994d938e237a5c668815226bc5ff1
Chan531 <chan657312@gmail.com>
2023-01-20 18:07:51 +0900

    메서드 이름 수정 및 when, given, then 지적에 대한 리뷰 반영

--- src/test/java/Model/NumberCheckTest.java
+++ src/test/java/Model/NumberCheckTest.java
@@ -12,15 +12,13 @@ class NumberCheckTest {
 
     @Test
     @DisplayName("음수, 0, 양수의 입력에 알맞게 대응하는지 테스트")
-    void canNumberCheck() {
+    void canCheckNumber() {
         // when
         int minus = -24;
         int zero = 0;
         int plus = 156;
 
-        // given
-
-        // then
+        // given, then
         assertThatThrownBy(() -> numberCheck.numberCheck(minus))
                 .isInstanceOf(IllegalStateException.class);
 
