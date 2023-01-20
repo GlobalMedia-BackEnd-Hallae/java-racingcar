57acd57dd930ea833d1d6bbeda911eb1c56ae44c
chanwoo <chocw0402@gmail.com>
2023-01-20 14:51:49 +0900

    refactor : 상수 변수명 변경 및 equalsAndHashCode 추가

--- src/main/java/gmbs/model/car/vo/Name.java
+++ src/main/java/gmbs/model/car/vo/Name.java
@@ -1,8 +1,10 @@
 package gmbs.model.car.vo;
 
+import java.util.Objects;
+
 public class Name {
 
-    private static final int CAR_NAME_MAX_LENGTH = 5;
+    private static final int MAX_LENGTH_OF_CAR_NAME = 5;
 
     private final String value;
 
@@ -11,8 +13,8 @@ public class Name {
         this.value = value;
     }
 
-    private void checkName(String value) throws IllegalArgumentException {
-        if (value.length() > CAR_NAME_MAX_LENGTH) {
+    private void checkName(String value) {
+        if (value.length() > MAX_LENGTH_OF_CAR_NAME) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자를 넘길 수 없습니다");
         }
         if (value.isBlank()) {
@@ -22,5 +24,18 @@ public class Name {
 
     public String getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name = (Name) o;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
