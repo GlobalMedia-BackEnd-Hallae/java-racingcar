d5cc0161395d2d0edddaef44a7352738cb943e29
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-23 23:41:02 +0900

    refactor : 정규식 사용

--- src/main/java/gmbs/model/Name.java
+++ src/main/java/gmbs/model/Name.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class Name {
     private static final int MAX_LENGTH = 5;
     private static final String EMPTY_NAME_MESSAGE = "[ERROR] 이름은 빈 값으로 입력할 수 없습니다.";
-    private static final String DUPLICATED_NAME_MESSAGE = "[ERROR] 5글자를 초과하는 이름이 존재합니다.";
+    private static final String LONG_NAME_MESSAGE = "[ERROR] 5글자를 초과하는 이름이 존재합니다.";
     private final String name;
 
     public Name(String name) {
@@ -13,17 +13,12 @@ public class Name {
         validate();
     }
 
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
     private void validate() {
-        validateNotEmpty();
+        validateNotBlank();
         validateLength();
     }
 
-    private void validateNotEmpty() {
+    private void validateNotBlank() {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException(EMPTY_NAME_MESSAGE);
         }
@@ -31,7 +26,7 @@ public class Name {
 
     private void validateLength() {
         if (name.length() > MAX_LENGTH) {
-            throw new IllegalArgumentException(DUPLICATED_NAME_MESSAGE);
+            throw new IllegalArgumentException(LONG_NAME_MESSAGE);
         }
     }
 
