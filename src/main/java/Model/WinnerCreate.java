7ba2a608f55cc5777f98bd0d05fd8c51c07c8695
Chan531 <chan657312@gmail.com>
2023-01-23 18:05:46 +0900

    Winner List를 내부에서 선언

--- src/main/java/Model/WinnerCreate.java
+++ src/main/java/Model/WinnerCreate.java
@@ -1,47 +1,23 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class WinnerCreate {
 
-    List<String> winner = new ArrayList<>();
+    private static final String EXPRESSION_DELIMITER = "";
 
     public List<String> createWinner(List<Car> carStatus) {
-        int maxDistance = maxDistance(carStatus);
-
-        for (Car car : carStatus) {
-            isWinner(maxDistance, car.carPosition(), car.carName());
-        }
-
-        return winner;
-    }
-
-    private int maxDistance(List<Car> carStatus) {
-        final int INITIAL_VALUE = 0;
-
-        int maxValue = INITIAL_VALUE;
-
-        for (Car car : carStatus) {
-            maxValue = Math.max(car.carPosition(), maxValue);
-        }
-
-        return maxValue;
-    }
-
-    private void isWinner(int maxDistance, int carDistance, String carName) {
-        if (maxDistance == carDistance) {
-            winner.add(carName);
-        }
-    }
-
-    /*public List<String> createWinner(List<Car> carStatus) {
         List<String> winner = new ArrayList<>();
+
         int maxDistance = maxDistance(carStatus);
 
         for (Car car : carStatus) {
             winner.add(isWinner(maxDistance, car.carPosition(), car.carName()));
         }
+
+        winner.removeAll(Arrays.asList(EXPRESSION_DELIMITER));
 
         return winner;
     }
@@ -63,6 +39,6 @@ public class WinnerCreate {
             return carName;
         }
 
-        return null;
-    }*/
+        return EXPRESSION_DELIMITER;
+    }
 }
