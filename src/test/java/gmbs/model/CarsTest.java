1400b6222ff8bd5047a9fcedb6d79bc4c093229a
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-23 23:37:29 +0900

    test : assertThatCode로 예외 발생하는지 체크

--- src/test/java/gmbs/model/CarsTest.java
+++ src/test/java/gmbs/model/CarsTest.java
@@ -5,27 +5,19 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
+
     @Test
     void 생성_성공() {
         // given
-        String names = "kim,jun,sep";
-
-        // when
-        Cars cars = new Cars(names);
-
-        // then
-        assertThat(cars).isNotNull();
-    }
-
-    @Test
-    void 중복_체크() {
-        // given
-        String names = "kim,kim,jun";
+        String names = "kim,jun,seop";
 
         // when
         // then
-        assertThatThrownBy(() -> new Cars(names)).isInstanceOf(IllegalArgumentException.class);
+        assertThatCode(() -> {
+            new Cars(names);
+        }).doesNotThrowAnyException();
     }
+
 
     @Test
     void toString_결과_생성() {
