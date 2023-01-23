8c7330892fd8866c0e368ea61836e90e4f2c2085
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-23 23:40:09 +0900

    refactor : 개행 추가

--- src/main/java/gmbs/Main.java
+++ src/main/java/gmbs/Main.java
@@ -1,5 +1,7 @@
 package gmbs;
+
 import gmbs.controller.RacingController;
+
 public class Main {
     public static void main(String[] args) {
         RacingController racingController = new RacingController();
