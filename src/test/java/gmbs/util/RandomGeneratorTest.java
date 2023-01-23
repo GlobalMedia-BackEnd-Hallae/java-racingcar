944864b795677e3b1b43d5e840343325926b4ddd
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-23 23:43:59 +0900

    refactor : 정규식 사용

--- src/test/java/gmbs/util/RandomGeneratorTest.java
+++ src/test/java/gmbs/util/RandomGeneratorTest.java
@@ -1,7 +1,9 @@
 package gmbs.util;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RandomGeneratorTest {
@@ -16,5 +18,18 @@ class RandomGeneratorTest {
 
         // then
         assertThat(number).isGreaterThanOrEqualTo(min).isLessThan(max);
+    }
+
+    @Test
+    @DisplayName("max가_더_작은_경우")
+    void less() {
+        // given
+        int min = 10;
+        int max = 0;
+
+        // when
+
+        // then
+        assertDoesNotThrow(() -> RandomGenerator.generateNumber(min, max));
     }
 }
