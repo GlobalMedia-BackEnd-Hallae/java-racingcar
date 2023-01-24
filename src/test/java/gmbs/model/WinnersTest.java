e0dcb3012ae9424490dc4602b0657a162da5686e
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-24 17:33:09 +0900

    test : 우승자 테스트 추가

--- src/test/java/gmbs/model/WinnersTest.java
+++ src/test/java/gmbs/model/WinnersTest.java
@@ -10,21 +10,57 @@ import org.junit.jupiter.api.Test;
 class WinnersTest {
 
     @Test
-    void 비어있지_않음() {
+    void 공동_우승자() {
         // given
         String name1 = "kim";
         String name2 = "jun";
-        List<Car> cars = new ArrayList<>();
-        Car car1 = new Car(name1);
-        Car car2 = new Car(name2);
 
-        cars.add(car1);
-        cars.add(car2);
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car(name1));
+        cars.add(new Car(name2));
 
         // when
         Winners winners = new Winners(cars);
 
         // then
-        assertThat(winners.toString()).contains(name1, name2);
+        assertThat(winners.getNames())
+                .contains(name1, name2);
+    }
+
+    @Test
+    void 우승자_1명() {
+        // given
+        String name1 = "kim";
+        String name2 = "jun";
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car(name1));
+
+        Car car1 = new Car(name2);
+        car1.move(6);
+        cars.add(car1);
+
+        // when
+        Winners winners = new Winners(cars);
+
+        // then
+        assertThat(winners.getNames())
+                .doesNotContain(name1);
+    }
+
+    @Test
+    void maxPosition() {
+        // given
+        List<Car> cars = new ArrayList<>();
+        Car car = new Car("kimjunseop");
+        car.move(6);
+        car.move(6);
+        cars.add(car);
+
+        // when
+        Winners winners = new Winners(cars);
+
+        // then
+        assertThat(winners.getMaxPosition()).isEqualTo(2);
     }
 }
